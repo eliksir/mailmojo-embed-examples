2334881f85b9216931a363197551056c5ef1cc43
Stian Prestholdt <stian@e5r.no>
2014-05-27 15:04:33 +0200

    Update description text and some minor CSS tweaks

--- demo/forms.py
+++ demo/forms.py
@@ -10,10 +10,10 @@ class TransparentIntegrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Try with the example "demoapi" user'
     }))
-    css = forms.BooleanField(label='Use custom CSS', required=False)
     lang = forms.ChoiceField(label='Choose language', choices=LANGUAGE, required=True)
     skip_recipients_step = forms.BooleanField(label='Skip recipients step',
             initial=True, required=False)
+    css = forms.BooleanField(label='Use custom CSS', required=False)
 
 
 class LoginIntegrationForm(forms.Form):
