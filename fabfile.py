1c10a32916959dc0aab61dc3c3a99e6feb105c25
Stian Prestholdt <stian@e5r.no>
2016-09-18 11:51:35 +0200

    Update fabfile with latest changes

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,8 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import os
 from contextlib import nested
 
-from fabric.api import *
+from fabric.api import *  # noqa
 from fabric.contrib.console import confirm
 from fabric.utils import abort
 
@@ -11,49 +11,49 @@ env.user = 'e5r'
 env.home = '/home/e5r/domains/mm-api-demo.e5r.no'
 env.hosts = ['heinlein.e5r.no']
 
+
 def deploy(branch='master'):
     """Lanser HEAD fra master i staging eller production miljøet."""
     require('environment', provided_by=('staging', 'production'))
     if env.environment == 'production':
         if not confirm("Er du sikker på at du vil lansere til 'production'?",
-                               default=False):
+                       default=False):
             abort("Lansering til 'production' kansellert.")
     pull(branch)
     update_requirements()
-    #migrate()
     collect_static()
     reload()
 
+
 def pull(branch):
     """Henter HEAD fra master."""
+
     require('root', provided_by=('staging', 'production'))
     with nested(cd(env.root), show('stdout')):
         run("git pull")
         run("git checkout -B {0} origin/{0}".format(branch))
 
+
 def update_requirements():
-    """Oppdater pakker definert i requirements.txt og node package.json."""
+    """Oppdater pakker definert i requirements.txt og node package.json.
+
+    TODO: Add npm install when we need to.
+    """
+
     require('root', provided_by=('staging', 'production'))
     with nested(cd(env.root), hide('stdout'),
                 prefix('source %(virtualenv)s/bin/activate' % env)):
         run("pip install -r requirements.txt")
-        run("npm install")
-        run("bower install")
 
-def migrate():
-    """Migrer databasen."""
-    require('root', provided_by=('staging', 'production'))
-    with nested(cd(env.root),
-                prefix('source %(virtualenv)s/bin/activate' % env)):
-        run("./manage.py migrate")
 
 def collect_static():
     """Kompilerer og komprimerer statiske ressurser før lansering."""
     require('root', provided_by=('staging', 'production'))
     with nested(cd(env.root),
                 prefix('source %(virtualenv)s/bin/activate' % env)):
-        run("grunt build")
+        # run("grunt build")
         run("yes yes | ./manage.py collectstatic")
+
 
 def reload():
     """Omstart webserveren."""
@@ -61,11 +61,13 @@ def reload():
     with nested(cd(env.home)):
         run("bin/stop.sh && bin/init.sh")
 
+
 def staging():
     """Forbereder bruk av 'staging' miljøet."""
     env.environment = 'staging'
     env.root = os.path.join(env.home, 'code/staging')
     env.virtualenv = os.path.join(env.home, '.virtualenvs/staging')
+
 
 def production():
     """Forbereder bruk av 'production' miljøet."""
